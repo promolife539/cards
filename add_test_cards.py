be28756565cd67f73965a2dd7222ea26e066720d
yulias <yulia@Yulias-MacBook-Air.local>
2017-07-08 15:55:30 +0300

    Flask cards app

---
+++ add_test_cards.py
@@ -0,0 +1,32 @@
+from db import db_session, Card
+
+cards = [
+    {
+        'en_meaning': 'Puppy',
+        'ru_meaning': 'Щенок',
+        'example': 'National charity, Hearing Dogs for Deaf People, which has a training centre at Cliffe, near Selby, is appealing for volunteers to take in puppies and young dogs for anything up to six weeks.',
+        'extra_info':'ˈpʌpi',
+        'is_active': 1,
+    },
+    {
+        'en_meaning': 'Squirrel',
+        'ru_meaning': 'Белка',
+        'example': 'Tree-dwelling squirrels have lovely bushy tails, and we realised that the little ones, from the look of them, were also from one such splendid family line.',
+        'extra_info':'ˈskwɪrəl',
+        'is_active': 1,
+    },
+    {
+        'en_meaning': 'Truck',
+        'ru_meaning': 'Грузовик',
+        'example': 'There will be classic and vintage cars, racing cars, go-carts, bikes, trucks , service vehicles and just about anything else with wheels.',
+        'extra_info':'trʌk',
+        'is_active': 1,
+    }      
+]
+
+# в цикле создаём объект card, это объект класса Card
+for c in cards:
+    card = Card(c['en_meaning'], c['ru_meaning'], c['example'], c['extra_info'], c['is_active'])
+    db_session.add(card)
+
+db_session.commit()
---
+++ db.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
+from sqlalchemy.orm import relationship, scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///cards_flask.sqlite')
+
+db_session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+# создали класс Card и наследовали его от класса Base (который нам создала алхимия)
+class Card(Base):
+    __tablename__ = 'cards'
+    id = Column(Integer, primary_key=True)
+    en_meaning = Column(String(500), unique=True)
+    ru_meaning = Column(String(500))
+    example = Column(String(500))
+    extra_info = Column(String(500))
+    is_active = Column(Boolean)
+
+    # метод, который вызывается автоматически, когда мы создаем новый объект класса Card
+    def __init__(self, en_meaning=None, ru_meaning=None, example=None, extra_info=None, is_active=None): 
+        self.en_meaning = en_meaning
+        self.ru_meaning = ru_meaning
+        self.example = example
+        self.extra_info = extra_info
+        self.is_active = is_active
+
+    def __repr__(self):
+        return '<Card {} {} {} {} {}>'.format(self.en_meaning, self.ru_meaning, self.example, self.extra_info, self.is_active)
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
