937c4823131126bfb61367f663a78c6a4d57c474
Alibek <promolife539@gmail.com>
2017-07-08 19:48:40 +0600

    Add static hosting

--- cardsite/settings.py
+++ cardsite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',
     'main',
 ]
 
@@ -121,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
